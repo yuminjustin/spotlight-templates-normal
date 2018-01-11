8627aed8ea7440e260df2c732274e39b126868eb
justin lee <yuminjustin@163.com>
2018-01-11 15:34:30 +0800

    图片地址优化

--- template/build/webpack.config.base.js
+++ template/build/webpack.config.base.js
@@ -22,7 +22,7 @@ module.exports = {
             loader: 'url-loader',
             options: {
                 limit: 10000,
-                name: utils.assetsPath('img/[name].[hash:7].[ext]')
+                name: utils.assetsPath('image/[name].[hash:7].[ext]')
             }
         },
         {
